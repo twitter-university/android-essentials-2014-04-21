661530d282548afe15f99788ab7d0611e74841aa
G. Blake Meike <bmeike@twitter.com>
2014-04-24 14:39:04 -0700

    Working timeline

--- app/src/main/java/com/twitter/university/android/yamba/TimelineFragment.java
+++ app/src/main/java/com/twitter/university/android/yamba/TimelineFragment.java
@@ -6,12 +6,17 @@ import android.content.CursorLoader;
 import android.content.Loader;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.text.format.DateUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
 
 import com.twitter.university.android.yamba.service.YambaContract;
+
+import org.w3c.dom.Text;
 
 /**
  * Created by bmeike on 4/24/14.
@@ -21,11 +26,31 @@ public class TimelineFragment extends ListFragment implements LoaderManager.Load
 
     private static final String[] FROM = new String[] {
             YambaContract.Timeline.Columns.HANDLE,
+            YambaContract.Timeline.Columns.TIMESTAMP,
+            YambaContract.Timeline.Columns.TWEET
     };
 
     private static final int[] TO = new int[] {
             R.id.timeline_handle,
+            R.id.timeline_timestamp,
+            R.id.timeline_tweet
     };
+
+     class TimelineBinder implements SimpleCursorAdapter.ViewBinder {
+
+        @Override
+        public boolean setViewValue(View view, Cursor cursor, int colId) {
+            if (view.getId() != R.id.timeline_timestamp) { return false; }
+
+            CharSequence tStr = "long ago";
+            long t = cursor.getLong(colId);
+            if (0 < t) { tStr = DateUtils.getRelativeTimeSpanString(t); }
+            ((TextView) view).setText(tStr);
+
+            return true;
+        }
+    }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle state) {
@@ -38,6 +63,7 @@ public class TimelineFragment extends ListFragment implements LoaderManager.Load
                 FROM,
                 TO,
                 0);
+        adapter.setViewBinder(new TimelineBinder());
         setListAdapter(adapter);
 
         getLoaderManager().initLoader(TIMELINE_LOADER, null, this);
@@ -58,6 +84,7 @@ public class TimelineFragment extends ListFragment implements LoaderManager.Load
 
     @Override
     public void onLoadFinished(Loader<Cursor> loader, Cursor c) {
+        Log.d("LOADER", "Got cursor: " + c);
         ((SimpleCursorAdapter) getListAdapter()).swapCursor(c);
     }
 
